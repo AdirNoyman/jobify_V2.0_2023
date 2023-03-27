8fa901762ae0371d92448fceacea1708082d4c07
AdirNoyman <ofahol2007@hotmail.com>
2023-03-27 22:02:27 +0300

    Added scripts

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,6 +1,20 @@
 import React from 'react';
+import { useEffect } from 'react';
 
 const Dashboard = () => {
+	const fetchData = async () => {
+		try {
+			const response = await fetch('http://localhost:5000/');
+			const data = await response.json();
+			console.log(data);
+		} catch (err) {
+			console.log(err);
+		}
+	};
+
+	useEffect(() => {
+		fetchData();
+	}, []);
 	return <h1>Dashboard</h1>;
 };
 
